06afc04ec0034ff83968db9416a31a196b56b671
alguna <o.roche@alguna.fr>
2021-03-18 17:10:45 +0100

    correction attributs

--- base/js/scripts.js
+++ base/js/scripts.js
@@ -1,14 +1,22 @@
 class TestComponent extends HTMLElement {
+  static get observedAttributes() {
+    return ["style"];
+  }
   constructor() {
     super();
     this.innerHTML = "<p>test</p>";
-    this.addEventListener("click", () => this.remove());
   }
   connectedCallback() {
     console.log("composant ajouté!");
+    this.addEventListener("click", () => (this.style.color = "blue"));
   }
   disconnectedCallback() {
     console.log("composant retiré!");
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name);
+    console.log(oldValue);
+    console.log(newValue);
   }
 }
 
