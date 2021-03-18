bb070a98b62dfa590fdb3159abe7f817a6a9c3fd
alguna <o.roche@alguna.fr>
2021-03-18 15:24:25 +0100

    paragraph

--- base/js/scripts.js
+++ base/js/scripts.js
@@ -12,14 +12,19 @@ class Car {
   status() {
     return this.brand + " running at " + this.speed + " km/h";
   }
+  updateParagraph() {
+    document.querySelector("p").innerText = this.status();
+  }
 }
 
 const renault = new Car("Renault");
 renault.accelerate(100);
 renault.brake(50);
 console.log(renault.status());
+renault.updateParagraph();
 
 const ferrari = new Car("Ferrari");
 ferrari.accelerate(50);
 ferrari.brake(25);
 console.log(ferrari.status());
+ferrari.updateParagraph();
