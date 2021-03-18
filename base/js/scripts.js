58c5e5da6a7341d454928a4445bab5501d08020d
alguna <o.roche@alguna.fr>
2021-03-18 17:26:40 +0100

    exemple complet et commenté

--- base/js/scripts.js
+++ base/js/scripts.js
@@ -1,23 +1,53 @@
+/**
+ * Informations piscines :
+ */
+const piscines = [
+  { city: "Paris", quantity: 84 },
+  { city: "Marseille", quantity: 29 },
+  { city: "Lyon", quantity: 14 },
+  { city: "Toulouse", quantity: 21 },
+];
+
+/**
+ * Classe pour chaque élément :
+ */
 class TestComponent extends HTMLElement {
   static get observedAttributes() {
     return ["style"];
   }
+  // Le constructeur avec super() qui fait référence à HTMLElement :
   constructor() {
     super();
-    this.innerHTML = "<p>test</p>";
   }
+  // méthode appelée lorsque l'élément est connecté :
   connectedCallback() {
     console.log("composant ajouté!");
     this.addEventListener("click", () => (this.style.color = "blue"));
+    this.innerHTML = `
+    <h2>${piscines[this.id].city}</h2>
+    <p>Nombre de piscines : ${piscines[this.id].quantity}</p>
+    `;
   }
+  // méthode appelée lorsque l'élément est déconnecté :
   disconnectedCallback() {
     console.log("composant retiré!");
   }
+  // méthode appelée lorsqu'un attribut est modifié, attention à utiliser static get observedAttributes() :
   attributeChangedCallback(name, oldValue, newValue) {
     console.log(name);
     console.log(oldValue);
     console.log(newValue);
   }
 }
-
+// Définition de l'élément :
 customElements.define("test-component", TestComponent);
+
+/**
+ * Boucle qui crée des éléments en fonction du tableau piscines :
+ */
+for (let piscine in piscines) {
+  const component = document.createElement("test-component");
+  // L'identifiant permet d'accèder à l'élément du tableau piscines :
+  component.id = piscine;
+  document.body.appendChild(component);
+}
